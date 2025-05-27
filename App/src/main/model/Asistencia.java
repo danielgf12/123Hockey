fe686ed3fc2c5e433cf7ab9a147e7588bffddfdf
danielgf12 <danielgf.zgz@gmail.com>
2025-05-27 12:21:57 +0200

    javadoc Asistencia.java

--- App/src/main/model/Asistencia.java
+++ App/src/main/model/Asistencia.java
@@ -2,6 +2,13 @@ package main.model;
 
 import javax.persistence.*;
 
+/**
+ * Entidad que representa la asistencia de un jugador a un entrenamiento.
+ * Usa una clave compuesta formada por el ID del entrenamiento y del usuario.
+ * 
+ * @author Daniel García
+ * @version 1.0
+ */
 @Entity
 @Table(name = "asistencia")
 public class Asistencia {
@@ -10,27 +17,35 @@ public class Asistencia {
     private AsistenciaId id;
 
     @ManyToOne
-    @MapsId("entrenamiento")      // coincide con el campo 'entrenamiento' del embeddable
+    @MapsId("entrenamiento")
     @JoinColumn(name = "id_entrenamiento")
     private Entrenamiento entrenamiento;
 
     @ManyToOne
-    @MapsId("usuario")            // coincide con el campo 'usuario' del embeddable
+    @MapsId("usuario")
     @JoinColumn(name = "id_usuario")
     private Usuario usuario;
 
     private boolean asistencia;
 
+    /**
+     * Constructor por defecto requerido por JPA.
+     */
     public Asistencia() {}
 
+    /**
+     * Constructor que inicializa la asistencia con los datos del entrenamiento, jugador y valor booleano.
+     *
+     * @param ent   Entrenamiento al que corresponde la asistencia
+     * @param usr   Usuario que asiste
+     * @param asist Valor de asistencia (true si asistió, false si no)
+     */
     public Asistencia(Entrenamiento ent, Usuario usr, boolean asist) {
         this.entrenamiento = ent;
         this.usuario       = usr;
         this.asistencia    = asist;
         this.id            = new AsistenciaId(ent.getId(), usr.getId());
     }
-
-    // Getters y Setters
 
     public AsistenciaId getId() { return id; }
 
