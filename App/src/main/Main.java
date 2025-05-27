d7f74c07284d64955768920f2a15ba4bb0ef258c
danielgf12 <danielgf.zgz@gmail.com>
2025-05-27 13:05:17 +0200

    javadoc Main.java

--- App/src/main/Main.java
+++ App/src/main/Main.java
@@ -2,9 +2,15 @@ package main;
 
 import main.view.LoginVentana;
 
+/**
+ * Clase principal de la aplicación.
+ * Lanza la interfaz de inicio de sesión.
+ * 
+ * @author Daniel García
+ * @version 1.0
+ */
 public class Main {
     public static void main(String[] args) {
         new LoginVentana();
     }
 }
-
