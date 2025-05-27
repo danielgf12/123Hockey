acf4f16aae58ad682db1a1fe9c2ed05756d6ddc6
danielgf12 <danielgf.zgz@gmail.com>
2025-05-27 12:20:43 +0200

    javadoc AlineacionId.java

--- App/src/main/model/AlineacionId.java
+++ App/src/main/model/AlineacionId.java
@@ -4,28 +4,40 @@ import javax.persistence.Embeddable;
 import java.io.Serializable;
 import java.util.Objects;
 
+/**
+ * Clase embebida que representa la clave primaria compuesta de la entidad Alineacion.
+ * Está formada por el ID del partido y el ID del usuario.
+ * 
+ * @author Daniel García
+ * @version 1.0
+ */
 @Embeddable
 public class AlineacionId implements Serializable {
 
     private int idPartido;
     private int idUsuario;
 
+    /**
+     * Constructor por defecto requerido por JPA.
+     */
     public AlineacionId() {}
 
+    /**
+     * Constructor con parámetros para inicializar la clave compuesta.
+     *
+     * @param idPartido ID del partido
+     * @param idUsuario ID del jugador
+     */
     public AlineacionId(int idPartido, int idUsuario) {
         this.idPartido = idPartido;
         this.idUsuario = idUsuario;
     }
-
-    // Getters y Setters
 
     public int getIdPartido() { return idPartido; }
     public void setIdPartido(int idPartido) { this.idPartido = idPartido; }
 
     public int getIdUsuario() { return idUsuario; }
     public void setIdUsuario(int idUsuario) { this.idUsuario = idUsuario; }
-
-    // equals y hashCode
 
     @Override
     public boolean equals(Object o) {
